4c6da5576c65683e6e0ce78e8d927a649624578c
stationAstral <cougarjonez@gmail.com>
2023-11-06 07:05:57 -0800

    1.1

--- listPractice/ContentView.swift
+++ listPractice/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  listPractice
+//  meesingAround
 //
 //  Created by stationAstral on 11/6/23.
 //
@@ -9,13 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .fill(Color.yellow)
+                .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    Rectangle() // Top-left corner rectangle
+                        .fill(Color.red)
+                        .frame(width: 50, height: 50)
+                    //connect line from one rectangle to another
+                    // add a list
+                    
+                    Spacer()
+                    Rectangle() // Top-left corner rectangle
+                        .fill(Color.cyan)
+                        .frame(width: 50, height: 50)
+                }
+        Spacer()
+                List {
+                    Text("Allie")
+                    Text("Mark")
+                    Text("Jameo")
+                    Text("Jameo")
+                    Text("Jameo")
+                    Text("Jameo")
+                    Text("Jameo")
+                }
+
+                .frame(width: 300, height: 300)
+                
+                Spacer()
+                
+                HStack{
+                    Rectangle() // Bottom-right corner rectangle
+                        .fill(Color.blue)
+                        .frame(width: 50, height: 50)
+                    Spacer()
+                    HStack{
+                        Rectangle() // Bottom-right corner rectangle
+                            .fill(Color.blue)
+                            .frame(width: 50, height: 50)
+                    }
+                    
+                }
+            }
+            .ignoresSafeArea()
         }
-        .padding()
     }
 }
 
